089df2a03e2a8e9e35ef34d15bc6298cb8084407
Yanjun Zhao <yanjunzhao81@gmail.com>
2023-08-09 20:05:42 -0400

    changed formatting of videos

--- src/components/Dojo_Locations.jsx
+++ src/components/Dojo_Locations.jsx
@@ -5,29 +5,29 @@ const Dojo_Locations = () => {
   return (
     <div className=' grid grid-cols-3 gap-4 w-full  '>       
       <div
-        className=' flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
+        className='col-span-full flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
         <p>List of AUSKF affiliated Kendo Dojos in the United States</p>
       </div>
-
-      <div className='col-span-2'>
+      <br></br>
+      <div className='col-span-full'>
         <iframe src="https://www.google.com/maps/d/embed?mid=1bdYRPtF1KRUd6ymUb4_Q1h7wSJ3X-E-I&ehbc=2E312F" width="50%" height="500"></iframe>
       </div>
-
+      <br></br>
       <div
-        className=' flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
+        className=' col-span-full flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
         <p>List of CKF affiliated Kendo Dojos in Canada</p>
       </div>
-
-      <div className='col-span-2'>
+      <br></br>
+      <div className='col-span-full'>
         <iframe src="https://www.google.com/maps/d/embed?mid=1nHSj-c735-i2IyeghohRn25IwP8&hl=en&ehbc=2E312F" width="640" height="500"></iframe>
       </div>
-
+      <br></br>
       <div
-        className=' flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
-        <p>List of EKF affiliated Kendo Dojos in Europe (scroll down a little and look up your country/city) </p>
+        className=' col-span-full flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
+        <p>List of EKF affiliated Kendo Dojos in Europe</p>
       </div>
-
-      <div className='col-span-2'>
+      <br></br>
+      <div className='col-span-full'>
         <iframe src="https://www.ekf-eu.com/index.php?page=dojos" width="200%" height="500" marginheight='200px'></iframe>
       </div>
       
--- src/components/WearingEquipment.jsx
+++ src/components/WearingEquipment.jsx
@@ -4,29 +4,29 @@ const WearingEquipment = () => {
   return (
     <div className=' grid grid-cols-3 gap-4 w-full  '>       
       <div
-        className=' flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
+        className='  col-span-full flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
         <span>Wearing your hakama & gi</span>
       </div>
-
-      <div className='col-span-2'>
+      <br></br>
+      <div className='col-span-full'>
       <iframe width="400" height="500" src="https://www.youtube.com/embed/JZaNrPULNxs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
-
+      <br></br>
       <div
-        className=' flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
+        className=' col-span-full flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
         <span>Attaching the strings (hemo) to your bogu</span>
       </div>
-
-      <div className='col-span-2'>
+      <br></br>
+      <div className='col-span-full'>
       <iframe width="400" height="500" src="https://www.youtube.com/embed/Z4PHt3H6dU0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
-
+      <br></br>
       <div
-        className=' flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
+        className=' col-span-full flex text-white sm:text-3xl text-lg font-comfortaa w-full text-center justify-center content-center'>
         <span>Puting on your bogu (armor) </span>
       </div>
-
-      <div className='col-span-2'>
+      <br></br>
+      <div className='col-span-full'>
       <iframe width="400" height="500" src="https://www.youtube.com/embed/DHXAmGiVWKs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     </div>
